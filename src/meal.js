9ecfd790cca3f9ac40f1898fafbab671e37e0c61
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-03-24 16:43:16 -0600

    Add implementation code

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,53 @@
-function nameMenuItem() {
-
+function nameMenuItem(food) {
+  return `Delicious ${food}`;
 }
 
+function createMenuItem (name,price,type){
+ var menuItem = {
+   name: (name),
+   price: (price),
+   type: (type)
+ };
+return menuItem;
+ }
 
+
+
+function addIngredients (ingredient,ingredients){
+  if (ingredients.includes(ingredient)){
+    return ("You fool! This ingredient has already been added.");
+  } else {
+    return ingredients.push(ingredient);
+ }
+}
+ 
+function formatPrice (price){
+  return `$${price}`;
+}
+
+function decreasePrice (markdownee){
+  var markdown = markdownee * .1;
+  return markdownee - markdown;
+}
+
+function createRecipe (title, ingredient, type) {
+  var recipe = {
+   title: (title),
+   ingredients: (ingredient),
+   type: (type),
+  };
+  return recipe;
+}
+
+createMenuItem()
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
+};
 
 
+// SHEESH! 
