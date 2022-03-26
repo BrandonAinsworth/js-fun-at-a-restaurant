01de8e6d548d771b0c11f153f2f6a88ded2f364c
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-03-25 22:45:18 -0600

    Add restaraunt

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,30 @@
+function createRestaurant(name) {
+ return {
+  name: (name),
+  menus: {
+  breakfast: [],
+  lunch: [],
+  dinner: [],
+  }
+};
+}
+function addMenuItem(ristorante, food){
+ 
+  if (ristorante.menus[food.type].includes(food)) {
+    console.log("This item has already been added") 
+  } else if (food.type == "lunch") {
+  return ristorante.menus.lunch.push(food) 
+  } else {
+return ristorante.menus.breakfast.push(food)
+  }
+}
 
+function removeMenuItem (ristorante, food, type) {
+  
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem,
 }
